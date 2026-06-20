3d7d1e9ee06244dd28712aa305d5d7cbcfe4210a
Soham <sohamgladdha@gmail.com>
2026-06-20 16:56:47 +0530

    debug cloudinary

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,5 +1,5 @@
 const cloudinary = require('cloudinary').v2;
-const multerStorageCloudinary = require('multer-storage-cloudinary');
+const {CloudinaryStorage} = require('multer-storage-cloudinary');
 
 
 cloudinary.config({
