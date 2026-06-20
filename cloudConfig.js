765eaf0f5a2a2896d88a1d7d73ffd77364b3e1a3
Soham <sohamgladdha@gmail.com>
2026-06-20 16:39:43 +0530

    debug cloudinary

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,5 +1,8 @@
 const cloudinary = require('cloudinary').v2;
 const CloudinaryStorage = require('multer-storage-cloudinary');
+console.log("CLOUD_NAME=",process.env.CLOUD_NAME)
+console.log("api-key",process.env.CLOU_DAPI_KEY)
+console.log("api secret=",process.env.CLOUD_SECRET_VALUE)
 
 cloudinary.config({
     cloud_name:process.env.CLOUD_NAME,
